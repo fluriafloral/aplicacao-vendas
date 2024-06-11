5f43c19f2e2a8b3316cfa870e0ac93ce5eec8469
fluriafloral <lampia131@gmail.com>
2024-06-10 21:55:28 -0300

    feat: add controllers

---
+++ src/controllers/crudController.js
@@ -0,0 +1,17 @@
+const getAllSales = (req, res) => {
+
+}
+
+const addNewSale = (req, res) => {
+
+}
+
+const updateSale = (req, res) => {
+
+}
+
+const deleteSale = (req, res) => {
+
+}
+
+module.exports = {getAllSales, addNewSale, updateSale, deleteSale}
---
+++ src/controllers/pdfController.js
@@ -0,0 +1,5 @@
+const getSalesPDF = (req, res) => {
+
+}
+
+module.exports = {getSalesPDF}
